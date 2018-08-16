7dc257c01bab4347b68ddb2fedfd88c8a96fc5a1
Steven Silvester <steven.silvester@ieee.org>
2018-08-16 06:06:54 -0500

    Refactor, refresh image

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ import {
 } from '@jupyterlab/apputils';
 
 import {
+  Message
+} from '@phosphor/messaging';
+
+import {
   Widget
 } from '@phosphor/widgets';
 
@@ -20,6 +24,112 @@ interface APODResponse {
   url: string;
 };
 
+class APODWidget extends Widget {
+  /**
+   * Construct a new APOD widget.
+   */
+  constructor() {
+    super();
+
+    this.addClass('my-apodWidget'); // new line
+
+    // Add an image element to the panel
+    this.img = document.createElement('img');
+    this.node.appendChild(this.img);
+
+    // Add a summary element to the panel
+    this.summary = document.createElement('p');
+    this.node.appendChild(this.summary);
+  }
+
+  /**
+   * The image element associated with the widget.
+   */
+  readonly img: HTMLImageElement;
+
+  /**
+   * The summary text element associated with the widget.
+   */
+  readonly summary: HTMLParagraphElement;
+
+  /**
+   * Handle update requests for the widget.
+   */
+  async onUpdateRequest(msg: Message): Promise<void> {
+
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${this.randomDate()}`);
+
+    if (!response.ok) {
+      const data = await response.json();
+      if (data.error) {
+        this.summary.innerText = data.error.message;
+      } else {
+        this.summary.innerText = response.statusText;
+      }
+      return;
+    }
+
+    const data = await response.json() as APODResponse;
+
+    if (data.media_type === 'image') {
+      // Populate the image
+      this.img.src = data.url;
+      this.img.title = data.title;
+      this.summary.innerText = data.title;
+      if (data.copyright) {
+        this.summary.innerText += ` (Copyright ${data.copyright})`;
+      }
+    } else {
+      this.summary.innerText = 'Random APOD fetched was not an image.';
+    }
+  }
+
+  /**
+   * Get a random date string in YYYY-MM-DD format.
+   */
+  randomDate(): string {
+    const start = new Date(2010, 1, 1);
+    const end = new Date();
+    const randomDate = new Date(start.getTime() + Math.random()*(end.getTime() - start.getTime()));
+    return randomDate.toISOString().slice(0, 10);
+  }
+}
+
+
+/**
+ * Activate the APOD widget extension.
+ */
+function activate(app: JupyterFrontEnd, palette: ICommandPalette) {
+  console.log('JupyterLab extension jupyterlab_apod is activated!');
+
+  // Create a single widget
+  const content = new APODWidget();
+  const widget = new MainAreaWidget({content});
+  widget.id = 'apod-jupyterlab';
+  widget.title.label = 'Astronomy Picture';
+  widget.title.closable = true;
+
+  // Add an application command
+  const command: string = 'apod:open';
+  app.commands.addCommand(command, {
+    label: 'Random Astronomy Picture',
+    execute: () => {
+      if (!widget.isAttached) {
+        // Attach the widget to the main work area if it's not there
+        app.shell.add(widget, 'main');
+      }
+      // Refresh the picture in the widget
+      content.update();
+      // Activate the widget
+      app.shell.activateById(widget.id);
+    }
+  });
+
+  // Add the command to the palette.
+  palette.addItem({ command, category: 'Tutorial' });
+}
+
+
 /**
  * Initialization data for the jupyterlab_apod extension.
  */
@@ -27,74 +137,7 @@ const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_apod',
   autoStart: true,
   requires: [ICommandPalette],
-  activate: async (app: JupyterFrontEnd, palette: ICommandPalette) => {
-    console.log('JupyterLab extension jupyterlab_apod is activated!');
-
-    // Create a blank content widget inside of a MainAreaWidget
-    const content = new Widget();
-    content.addClass('my-apodWidget'); // new line
-    const widget = new MainAreaWidget({content});
-    widget.id = 'apod-jupyterlab';
-    widget.title.label = 'Astronomy Picture';
-    widget.title.closable = true;
-
-    // Add an image element to the content
-    let img = document.createElement('img');
-    content.node.appendChild(img);
-
-    let summary = document.createElement('p');
-    content.node.appendChild(summary);
-
-    // Get a random date string in YYYY-MM-DD format
-    function randomDate() {
-      const start = new Date(2010, 1, 1);
-      const end = new Date();
-      const randomDate = new Date(start.getTime() + Math.random()*(end.getTime() - start.getTime()));
-      return randomDate.toISOString().slice(0, 10);
-    }
-
-    // Fetch info about a random picture
-    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${randomDate()}`);
-    if (!response.ok) {
-      const data = await response.json();
-      if (data.error) {
-        summary.innerText = data.error.message;
-      } else {
-        summary.innerText = response.statusText;
-      }
-    } else {
-      const data = await response.json() as APODResponse;
-
-      if (data.media_type === 'image') {
-        // Populate the image
-        img.src = data.url;
-        img.title = data.title;
-        summary.innerText = data.title;
-        if (data.copyright) {
-          summary.innerText += ` (Copyright ${data.copyright})`;
-        }
-      } else {
-        summary.innerText = 'Random APOD fetched was not an image.';
-      }
-    }
-
-    // Add an application command
-    const command: string = 'apod:open';
-    app.commands.addCommand(command, {
-      label: 'Random Astronomy Picture',
-      execute: () => {
-        if (!widget.isAttached) {
-          // Attach the widget to the main work area if it's not there
-          app.shell.add(widget, 'main');
-        }
-        // Activate the widget
-        app.shell.activateById(widget.id);
-      }
-    });
-
-    // Add the command to the palette.
-    palette.addItem({command, category: 'Tutorial'});
-  }
+  activate: activate
 };
 
 export default extension;
