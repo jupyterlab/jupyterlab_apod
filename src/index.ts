2eb0121ce78442fd250b81eeb98ce85d1df0769a
Steven Silvester <steven.silvester@ieee.org>
2018-08-16 06:03:49 -0500

    Show a picture in the panel

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,15 @@ import {
 } from '@phosphor/widgets';
 
 
+interface APODResponse {
+  copyright: string;
+  date: string;
+  explanation: string;
+  media_type: 'video' | 'image';
+  title: string;
+  url: string;
+};
+
 /**
  * Initialization data for the jupyterlab_apod extension.
  */
@@ -18,7 +27,7 @@ const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_apod',
   autoStart: true,
   requires: [ICommandPalette],
-  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
+  activate: async (app: JupyterFrontEnd, palette: ICommandPalette) => {
     console.log('JupyterLab extension jupyterlab_apod is activated!');
 
     // Create a blank content widget inside of a MainAreaWidget
@@ -27,6 +36,30 @@ const extension: JupyterFrontEndPlugin<void> = {
     widget.id = 'apod-jupyterlab';
     widget.title.label = 'Astronomy Picture';
     widget.title.closable = true;
+
+    // Add an image element to the content
+    let img = document.createElement('img');
+    content.node.appendChild(img);
+
+    // Get a random date string in YYYY-MM-DD format
+    function randomDate() {
+      const start = new Date(2010, 1, 1);
+      const end = new Date();
+      const randomDate = new Date(start.getTime() + Math.random()*(end.getTime() - start.getTime()));
+      return randomDate.toISOString().slice(0, 10);
+    }
+
+    // Fetch info about a random picture
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${randomDate()}`);
+    const data = await response.json() as APODResponse;
+
+    if (data.media_type === 'image') {
+      // Populate the image
+      img.src = data.url;
+      img.title = data.title;
+    } else {
+      console.log('Random APOD was not a picture.');
+    }
 
     // Add an application command
     const command: string = 'apod:open';
